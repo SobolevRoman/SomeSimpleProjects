676f82b6d65c23ae22b767ce0c39eb48ae98a17a
SobolevRoman <franki-link@mail.ru>
2022-06-09 14:29:09 +0300

    Implementation of game snake

---
+++ Games/src/ru/company/games/snake/Snake.java
@@ -0,0 +1,104 @@
+package ru.company.games.snake;
+
+import java.util.ArrayList;
+
+public class Snake {
+    private SnakeDirection direction;
+    private boolean isAlive;
+    private final ArrayList<SnakeSection> sections;
+
+    public Snake(int x, int y) {
+        sections = new ArrayList<SnakeSection>();
+        sections.add(new SnakeSection(x, y));
+        isAlive = true;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public int getX() {
+        return sections.get(0).getX();
+    }
+
+    public int getY() {
+        return sections.get(0).getY();
+    }
+
+    public SnakeDirection getDirection() {
+        return direction;
+    }
+
+    public void setDirection(SnakeDirection direction) {
+        this.direction = direction;
+    }
+
+    public ArrayList<SnakeSection> getSections() {
+        return sections;
+    }
+
+    /**
+     * Метод перемещает змею на один ход.
+     * Направление перемещения задано переменной direction.
+     */
+    public void move() {
+        if (!isAlive) return;
+
+        if (direction == SnakeDirection.UP)
+            move(0, -1);
+        else if (direction == SnakeDirection.RIGHT)
+            move(1, 0);
+        else if (direction == SnakeDirection.DOWN)
+            move(0, 1);
+        else if (direction == SnakeDirection.LEFT)
+            move(-1, 0);
+    }
+
+    /**
+     * Метод перемещает змею в соседнюю клетку.
+     * Координаты клетки заданы относительно текущей головы с помощью переменных (dx, dy).
+     */
+    private void move(int dx, int dy) {
+        //Создаем новую голову - новый "кусочек змеи".
+        SnakeSection head = sections.get(0);
+        head = new SnakeSection(head.getX() + dx, head.getY() + dy);
+
+        //Проверяем - не вылезла ли голова за границу комнаты
+        checkBorders(head);
+        if (!isAlive) return;
+
+        //Проверяем - не пересекает ли змея  саму себя
+        checkBody(head);
+        if (!isAlive) return;
+
+        //Проверяем - не съела ли змея мышь.
+        Mouse mouse = Room.game.getMouse();
+        if (head.getX() == mouse.getX() && head.getY() == mouse.getY()) //съела
+        {
+            sections.add(0, head);
+            Room.game.eatMouse();
+        } else //просто движется
+        {
+            sections.add(0, head);
+            sections.remove(sections.size() - 1);
+        }
+    }
+
+    /**
+     * Метод проверяет - находится ли новая голова в пределах комнаты
+     */
+    private void checkBorders(SnakeSection head) {
+        if ((head.getX() < 0 || head.getX() >= Room.game.getWidth()) || head.getY() < 0 || head.getY() >= Room.game.getHeight()) {
+            isAlive = false;
+        }
+    }
+
+    /**
+     * Метод проверяет - не совпадает ли голова с каким-нибудь участком тела змеи.
+     */
+    private void checkBody(SnakeSection head) {
+        if (sections.contains(head)) {
+            isAlive = false;
+        }
+    }
+}
---
+++ Games/src/ru/company/games/snake/SnakeDirection.java
@@ -0,0 +1,8 @@
+package ru.company.games.snake;
+
+public enum SnakeDirection {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+}
